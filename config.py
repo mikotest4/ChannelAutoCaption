bf0afe20284ac44876a8c10e321b5b73bb6090cc
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-07 06:31:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    DB_URL = os.environ.get("DATABASE_URL", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7845096754:AAGp_ydOCzsjrC89b3LOWHzuWG4gKTdK2uI")
+    API_ID = int(os.environ.get("API_ID", 29245477))
+    API_HASH = os.environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
+    DB_URL = os.environ.get("DATABASE_URL", "postgresql+asyncpg://neondb_owner:npg_XbwV4SzKFn7G@ep-steep-feather-a8wgninx-pooler.eastus2.azure.neon.tech/neondb")
